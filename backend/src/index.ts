63a6abcc496e9e912f2e2af2aa7a4adcd5edf39b
Zeeshan Bhati <zeeshanbhati99@gmail.com>
2022-12-08 01:14:01 +0530

    Build #2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-
+ 
 app.get('/drafts', async (req, res) => {
   const posts = await prisma.post.findMany({
     where: { published: false },
